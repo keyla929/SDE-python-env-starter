77d908aee6496d4f6534311e47f300fb52fb2c33
keyla929 <keylacastillo929@gmail.com>
2025-06-04 17:31:50 -0500

    Update DoNow.py

---
+++ Lesson2/DoNow.py
@@ -0,0 +1,9 @@
+#You want the function below to print a greeting message to the screen.
+# This code will not be executed until it is 'called'.
+def say_hello():
+  print('Hello!')  #change Hello to your favorite greeting
+
+
+# Our function is defined above, so now we can 'call' it
+say_hello()
+def function_name():
